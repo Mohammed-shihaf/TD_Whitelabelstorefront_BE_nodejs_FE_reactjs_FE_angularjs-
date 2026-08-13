488a02eabc0fe3318415bcc0a8ed4e5d28ce6c19
Mohammed-shihaf <shihaf@Testable.Cloud>
2026-08-13 18:17:36 +0530

    whitelabelstorefront_BE_nodejs22_FE_reactjs18_FE_angularjs17: add shell script (bash-version-guarded), IaC (Dockerfile+k8s+Terraform), and SOC2 doc for readiness scanner

---
+++ infra/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "whitelabelstorefront_backend" {
+  name = "whitelabelstorefront-backend:node22"
+  build {
+    context    = "${path.module}/.."
+    dockerfile = "infra/Dockerfile"
+  }
+}
+
+resource "docker_container" "whitelabelstorefront_backend" {
+  name  = "whitelabelstorefront-backend"
+  image = docker_image.whitelabelstorefront_backend.image_id
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
